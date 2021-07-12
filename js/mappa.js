3031215ec5803f75d9e2727ece7d325c14ffacf7
Verga-B <62094659+Verga-B@users.noreply.github.com>
2021-07-13 01:53:38 +0200

    Add files via upload

--- js/mappa.js
+++ js/mappa.js
@@ -1,20 +1,33 @@
-var map;
-        $(document).ready(function(){
-          map = new GMaps({
-            el: '#map',
-            lat: 45.497046,
-            lng: 9.643638
-          });
+function initMap() {
+    const uluru = { lat: 45.497046, lng: 9.643638 };
+    const map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 15,
+        center: uluru,
 
-          /* MARKER */
-          map.addMarker({
-            lat: 45.496214,
-            lng: 9.643378,
-            title: 'Marker with InfoWindow',
-            infoWindow: {
-              content: '<a onclick="fullboximg("../immagini/foto_es.jpeg", 365, 455, 15, 2); return false;" href="../immagini/foto_es.jpeg"> <img alt="demo" src="../immagini/foto_es.jpeg" class="foto_marker"/></a><p class="testo_marker">Via Roma</p>'
-            }
+    });
+    const cestini = [
+        [{ lat: 45.497046, lng: 9.643638 }],
+        [{ lat: 34.8559195, lng: -111.7988186 }],
+        [{ lat: 34.832149, lng: -111.7695277 }],
+        [{ lat: 34.823736, lng: -111.8001857 }],
+        [{ lat: 34.800326, lng: -111.7665047 }],
+      ];
+    // Create an info window to share between markers.
+    const infoWindow = new google.maps.InfoWindow();
+
+    //Markers
+    cestini.forEach(([position]) => {
+        const marker = new google.maps.Marker({
+            position,
+            map,
+            optimized: false,
           });
+        // Add a click listener for each marker, and set up the info window.
+        marker.addListener("click", () => {
+            infoWindow.close();
+            infoWindow.setContent('<h1>CIAO</h1>');
+            infoWindow.open(marker.getMap(), marker);
         });
-        
-
+    });
+    
+}
