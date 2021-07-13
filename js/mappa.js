307b95e5ebd86e3385586ecc6845ffa972659c3f
Verga-B <62094659+Verga-B@users.noreply.github.com>
2021-07-13 02:37:26 +0200

    Add files via upload

--- js/mappa.js
+++ js/mappa.js
@@ -1,31 +1,32 @@
 function initMap() {
-    const uluru = { lat: 45.497046, lng: 9.643638 };
+    const caravaggio = { lat: 45.497046, lng: 9.643638 };
     const map = new google.maps.Map(document.getElementById('map'), {
         zoom: 15,
-        center: uluru,
+        center: caravaggio,
 
     });
     const cestini = [
-        [{ lat: 45.497046, lng: 9.643638 }],
-        [{ lat: 34.8559195, lng: -111.7988186 }],
-        [{ lat: 34.832149, lng: -111.7695277 }],
-        [{ lat: 34.823736, lng: -111.8001857 }],
-        [{ lat: 34.800326, lng: -111.7665047 }],
+        [{ lat: 45.497046, lng: 9.643638 }, "../immagini/foto_es.jpeg"],
+        [{ lat: 34.8559195, lng: -111.7988186 }, ""],
+        [{ lat: 34.832149, lng: -111.7695277 }, ""],
+        [{ lat: 34.823736, lng: -111.8001857 }, ""],
+        [{ lat: 34.800326, lng: -111.7665047 }, ""],
       ];
     // Create an info window to share between markers.
     const infoWindow = new google.maps.InfoWindow();
 
     //Markers
-    cestini.forEach(([position]) => {
+    cestini.forEach(([position, content]) => {
         const marker = new google.maps.Marker({
             position,
             map,
+            title: content,
             optimized: false,
           });
         // Add a click listener for each marker, and set up the info window.
         marker.addListener("click", () => {
             infoWindow.close();
-            infoWindow.setContent('<h1>CIAO</h1>');
+            infoWindow.setContent('<img src=' + marker.getTitle() + ' alt="foto cestino" class="foto_marker">');
             infoWindow.open(marker.getMap(), marker);
         });
     });
