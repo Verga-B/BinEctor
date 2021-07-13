2bf82290f2ac3e5b23788ccb06575c3f2403fe51
Verga-B <62094659+Verga-B@users.noreply.github.com>
2021-07-13 02:45:17 +0200

    Update Versione.js

--- js/Versione.js
+++ js/Versione.js
@@ -1,4 +1,4 @@
 //Cambiamento  automatico della versione in tutte le pagine
 
-var versione = "0.3";
+var versione = "0.4";
 document.getElementById("vers").innerHTML = "Created by Verga Wiktor <br> Version " + versione;
